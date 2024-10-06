5c7caba9f996be15c6d878b80f24cc285d0a44ce
Siampaa <123250596+Siampaa@users.noreply.github.com>
2024-10-06 23:16:56 +0400

    Add files via upload

---
+++ vector.cpp
@@ -0,0 +1,11 @@
+﻿#include <vector>
+
+using namespace std;
+
+vector<int> incrementVector(const vector<int>& vec, int n) {
+    vector<int> result(vec.size());
+    for (size_t i = 0; i < vec.size(); i++) {
+        result[i] = vec[i] + n;
+    }
+    return result;
+}
