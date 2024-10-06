53db0f27163240ff20720eb0e1f2e0e5dba8a6d4
Siampaa <123250596+Siampaa@users.noreply.github.com>
2024-10-06 23:19:43 +0400

    Add files via upload

---
+++ google test dlya vectora.cpp
@@ -0,0 +1,17 @@
+﻿#include <gtest/gtest.h>
+#include "vector_increment.cpp"
+
+using namespace std;
+
+TEST(VectorIncrementTest, IncrementsVectorByN) {
+    vector<int> input = { 1, 2, 3, 4, 5 };
+    int n = 3;
+    vector<int> expected = { 4, 5, 6, 7, 8 };
+    vector<int> result = incrementVector(input, n);
+    ASSERT_EQ(result, expected);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
